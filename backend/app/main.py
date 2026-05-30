2907bafd8f6b3828d45ca3e5a372ebfd70f5ad6c
dylsdyl <dylanportelacavalcante@gmail.com>
2026-05-29 23:45:19 -0300

    Fix:liga o backend ao front end

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,45 +5,39 @@ from fastapi.staticfiles import StaticFiles
 from app.database.connection import engine
 from app.database.base import Base
 
+from app.routes import publicacao_routes, horta_routes, auth_routes
 
-# rotas
-from app.routes import publicacao_routes
-from app.routes import horta_routes
-from app.routes import auth_routes
-
-# models
 from app.models.admin_model import Admin
 from app.models.publicacao_model import Publicacao
 from app.models.horta_model import PublicacaoHorta
 
 
-
 app = FastAPI()
-app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
 
+# CORS (SÓ UMA VEZ)
 app.add_middleware(
     CORSMiddleware,
-
     allow_origins=[
-        "http://localhost:5173"
+        "http://localhost:3000",
+        "http://localhost:5173",
+        "https://ubs-8-taguatinga.vercel.app"  # CONFIRMA ISSO NO VERCEL
     ],
-
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
+app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
+
 # cria tabelas
 Base.metadata.create_all(bind=engine)
 
-# registra rotas
+# rotas
 app.include_router(publicacao_routes.router)
 app.include_router(horta_routes.router)
 app.include_router(auth_routes.router)
 
+
 @app.get("/")
 def home():
-
-    return {
-        "mensagem": "API UBS funcionando"
-    }
+    return {"mensagem": "API UBS funcionando"}
